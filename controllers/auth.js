fe3ef8f9e5a3328076ab5dfe0c703e52986b3845
Gabriel1Maximiliano <gabrielhmloza@gmail.com>
2023-03-22 23:17:59 -0300

    fields validation with express-validator in registerUser

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,5 @@
 const { response } = require("express");
-const { validationResult } = require("express-validator");
+
 
 
 const createUser = async( req, res = response )=>{
@@ -16,22 +16,14 @@ const createUser = async( req, res = response )=>{
 
 const loginUser = async( req, res = response )=>{
 
-    const errors = validationResult( req );
-
-    if( !errors.isEmpty() ){
-
-        return res.status(400).json({
-            ok:false,
-            errors:errors.mapped(),
-        })
-    }
+    
    
     const { email, password } = req.body;
 
     return res.json({
         ok:true,
         msg:'login user',
-    })
+    })  
 };
 
 const renewToken = async( req, res = response )=>{
--- router/auth.js
+++ router/auth.js
@@ -5,14 +5,25 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 const { createUser, renewToken, loginUser } = require('../controllers/auth');
+const { validateFields } = require('../middlewares/validateFields');
+
 
 const router = Router();
-
-router.post('/newUser', createUser)
+ 
+router.post('/newUser',[
+    check('name','The name is required').not().isEmpty(),
+    check('name','At least 2 characters in name field').isLength({ min: 2 }),
+    check('email','The name is required').isEmail(),
+    check('password','The password is required').not().isEmpty(),
+    check('password','At least 3 characters').isLength({ min: 3 }),
+    validateFields 
+] ,createUser)
 
 router.post('/loginUser',[
     check('email','The name is required').isEmail(),
     check('password','The password is required').not().isEmpty(),
+    check('password','At least 3 characters').isLength({ min: 3 }),
+    validateFields
 ] ,loginUser );
 
 router.get('/renewToken', renewToken);
