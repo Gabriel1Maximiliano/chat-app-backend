bbcb7dc5f0470489bda7cdbde2d24fa19b6ae13c
Gabriel1Maximiliano <gabrielhmloza@gmail.com>
2023-03-22 21:59:40 -0300

    Message model added

---
+++ models/users.js
@@ -0,0 +1,36 @@
+const { model, Schema } = require('mongoose');
+
+
+const UserSchema = Schema({
+
+    name:{
+        type:String,
+        require:true,
+    },
+    email:{
+        type:String,
+        require:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        require:true,
+    },
+    onLine:{
+        type:Boolean,
+        default:false,
+    },
+    
+    
+});
+
+UserSchema.method('toJSON', function(){
+
+    const { __v, _id, password, ...object } = this.toObject();
+
+    object.uid = _id;
+
+    return object;
+});
+
+module.exports = model('User', UserSchema);
