ef885bf58d73f08b1e5de036defc3fbbb14dc00e
Gabriel1Maximiliano <gabrielhmloza@gmail.com>
2023-04-07 13:06:52 -0300

    nuevo commit

--- controllers/sockets.js
+++ controllers/sockets.js
@@ -1,5 +1,5 @@
 const User = require('../models/Users');
-
+const Message = require('../models/Message');
 const userConnected = async( uid='' )=>{
     
     const user = await User.findById( uid );
@@ -33,10 +33,25 @@ const getUsers = async () =>{
     return users;
 }
 
+const recordMessage = async( payload ) =>{
+
+    try {
+
+        const message = new Message( payload );;
+        await message.save();
+        return message;
+        
+    } catch (error) {
+ console.log(error);
+        return false;
+    }
+
+}  
 
 
-module.exports = { 
+module.exports = {  
     userConnected,
     userDisconnected,
-    getUsers,  
+    getUsers,
+    recordMessage  
 };
--- models/sockets.js
+++ models/sockets.js
@@ -1,4 +1,4 @@
-const { userConnected,userDisconnected,getUsers } = require('../controllers/sockets');
+const { userConnected,userDisconnected,getUsers,recordMessage } = require('../controllers/sockets');
 const { verifyJwt } = require('../helpers/jwt');
 
 const TicketList = require('./ticket-list');
@@ -30,7 +30,12 @@ class Sockets {
            
          
 
- 
+         //TODO Socket join uid
+
+         socket.join( uid );
+
+
+       
 
           //TODO Emitir todos los usuarios conectados
           // console.log((await getUsers())
@@ -38,10 +43,17 @@ class Sockets {
           this.io.emit( 'list-users',(await getUsers())) 
    
 
-          //TODO Socket join uid
 
           //TODO Escuchar cuando el cliente manda un mensaje 
           // 'personal-message'
+
+          socket.on('personal-message', async( payload ) =>{
+
+           const message = await recordMessage( payload )
+          
+           this.io.to( payload.to ).emit( 'personal-message', message );
+           this.io.to( payload.from ).emit( 'personal-message', message );
+          } )
 
           //TODO Disconnect
           // marcar en la DB que el usuario se desconecto
